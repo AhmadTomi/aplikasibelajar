4c35d1d6824c29e92aceb007f4b16789d6e34728
Ahmad Tomi <nearueki@gmail.com>
2022-06-26 15:53:07 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/ahmadtomi/aplikasibelajar/aplikasibelajar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahmadtomi.aplikasibelajar.aplikasibelajar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/r.dart
@@ -0,0 +1,10 @@
+import '/constants/R/app_colors.dart';
+import '/constants/R/assets.dart';
+import '/constants/R/strings.dart';
+
+class R {
+  static AppColor colors = AppColor();
+  static Assets assets = Assets();
+  static Strings strings = Strings();
+  
+}
